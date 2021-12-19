a341691f44908e547980c9235e42511d8d5ca5a3
Marcelo Santos <riquecelox462m@gmail.com>
2021-12-19 06:28:33 -0300

    lógica da requisição
    usando fetch para obter o arquivo json

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 var botaoVerificardor = document.querySelector('#btn');
 botaoVerificardor.addEventListener('click', function(e){
     e.preventDefault();
-    alert('Olá Mundo!')
+    //alert('Olá Mundo!')
+
+    const requisicao = fetch("https://covid.ourworldindata.org/data/owid-covid-data.json")
+    .then(response => {
+        return response.json(); 
+    })
+    .then(jsonBody => {
+        console.log(jsonBody)
+    })
+    
 })
