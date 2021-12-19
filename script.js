042d53da732ea288dde5aec74a899163e83e4128
Marcelo Santos <riquecelox462m@gmail.com>
2021-12-19 06:52:49 -0300

    adicionando o input na busca
    adicionando o input na busca do resultado

--- script.js
+++ script.js
@@ -3,13 +3,17 @@ botaoVerificardor.addEventListener('click', function(e){
     e.preventDefault();
     //alert('Olá Mundo!')
 
+    let inputVerif1 = document.querySelector('input').value;
+    //console.log(inputVerif1)
+
     const requisicao = fetch("https://covid.ourworldindata.org/data/owid-covid-data.json")
     .then(response => {
         return response.json(); 
     })
     .then(jsonBody => {
-        document.querySelector('#res').innerHTML = jsonBody.OWID_WRL.data[2].total_cases
-        console.log(typeof jsonBody)
+        let inputVerif2 = inputVerif1;
+        document.querySelector('#res').innerHTML = jsonBody.OWID_WRL.data[inputVerif2].total_cases;
+        console.log(jsonBody)
     })
     
 })
